103340e22b77a06992fcd1d11b62bd480c9cf6e4
Maxwell Stennett <orfeeus@hotmail.com>
2018-03-19 12:33:46 -0400

    calculator - wip

---
+++ src/Calculator.js
@@ -0,0 +1,150 @@
+import React from 'react';
+import './App.css';
+
+class Calculator extends React.Component {
+
+  state = {
+      total: 0,
+      keyPadSum: '',
+      display: 0,
+      action: ''
+  }
+  componentWillMount() {
+    this.setState({
+      total: this.state.total,
+      keyPadSum: this.state.keyPadSum,
+      display: this.state.display,
+      action: this.state.action
+    })
+    console.log('componentWillMount:', this.state);
+  }  
+
+clearkeyPadSum = () => {
+  this.setState({
+  keyPadSum: ''
+  })  
+}
+add = (num) => {
+  this.setState({
+  total:  (parseInt(this.state.total,10) + parseInt(this.state.keyPadSum,10)),
+  action: 'add'
+  })
+  this.clearkeyPadSum();
+  console.log(this.state);
+}
+subtract = (num) => {
+  this.setState({
+  total: parseInt(this.state.total,10) - parseInt(this.state.keyPadSum,10),
+  action: 'subtract'
+  })
+  this.clearkeyPadSum();
+  console.log(this.state);
+
+}
+multiply = (num) => {
+  this.setState({
+  total: parseInt(this.state.keyPadSum,10) * parseInt(this.state.total,10),
+  action: 'multiply'
+  })
+  this.clearkeyPadSum();
+  console.log(this.state);
+}
+divide = (num) => {
+  this.setState({
+  total: parseInt(this.state.total,10) / parseInt(this.state.keyPadSum,10),
+  action: 'divide'
+  })
+  this.clearkeyPadSum();
+  console.log(this.state);
+}
+equal = () => {
+  if (this.state.action === 'add'){
+    this.setState({
+      total:  parseInt(this.state.total,10) + parseInt(this.state.keyPadSum,10)
+    })
+  }
+  if (this.state.action === 'subtract'){
+    this.setState({
+      total: (parseInt(this.state.total,10) - parseInt(this.state.keyPadSum,10))
+    })
+  }
+  if (this.state.action === 'multiply'){
+    this.setState({
+      total: (parseInt(this.state.keyPadSum,10) * parseInt(this.state.total,10))
+    })
+  }
+  if (this.state.action === 'divide'){
+    this.setState({
+      total: (parseInt(this.state.total,10) / parseInt(this.state.keyPadSum,10))
+    })
+  }
+  this.updateDisplay('total'); 
+  console.log(this.state);
+}
+clearCalculator = (event) => {
+  this.setState({
+    total: 0,
+    keyPadSum: '',
+    display: 0,
+    action: ''
+  })
+  console.log(this.state);
+}
+
+pushKeyPad = event => {
+  this.setState({
+    keyPadSum: this.state.keyPadSum += event.currentTarget.textContent 
+  })
+  this.updateDisplay('keyPadSum');
+  console.log(this.state);
+}
+
+updateDisplay = (update) => {
+  if (update === 'total'){
+    this.setState({
+      display: this.state.total
+    })
+    return;
+  }
+  this.setState({
+    display: this.state.keyPadSum
+  })
+}
+  
+  render() {
+    return (
+        <section className="c-cal-section">
+
+          <div className="c-cal-wrapper">
+            <div className="c-cal_display">{this.state.display}</div> 
+
+            <button onClick={this.clearCalculator} className="btn btn--clear">ac</button>
+            <button className="btn btn--pos-neg">+/-</button>
+            <button className="btn btn--percent">&#37;</button>
+            <button onClick={this.pushKeyPad} className="btn btn--period">&#46;</button>
+          
+            <button onClick={this.pushKeyPad} className="btn btn--one">1</button>
+            <button onClick={this.pushKeyPad} className="btn btn--two">2</button>
+            <button onClick={this.pushKeyPad} className="btn btn--three">3</button>
+            <button onClick={this.pushKeyPad} className="btn btn--four">4</button>
+            <button onClick={this.pushKeyPad} className="btn btn--five">5</button>
+            <button onClick={this.pushKeyPad} className="btn btn--six">6</button>
+            <button onClick={this.pushKeyPad} className="btn btn--seven">7</button>
+            <button onClick={this.pushKeyPad} className="btn btn--eight">8</button>
+            <button onClick={this.pushKeyPad} className="btn btn--nine">9</button>
+            <button onClick={this.pushKeyPad} className="btn btn--zero">0</button>
+            
+            <button onClick={this.divide} className="btn btn--divide">&#247;</button>
+            <button onClick={this.multiply} className="btn btn--multiply">&#215;</button>
+            <button onClick={this.subtract} className="btn btn--subtract">&#8722;</button>
+            <button onClick={this.add} className="btn btn--add">&#43;</button>
+            <button onClick={this.equal} className="btn btn--equal">&#61;</button>
+          </div>
+        </section>
+
+    
+    );
+  }
+}
+
+export default Calculator;

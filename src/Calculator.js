d77f65e9cadf3c4c24a140b5d8aad83ca0d54b20
Maxwell Stennett <orfeeus@hotmail.com>
2018-03-19 15:04:13 -0400

    add name attr to markup

--- src/Calculator.js
+++ src/Calculator.js
@@ -114,25 +114,25 @@ updateDisplay = (update) => {
         <section className="c-cal-section">
           <div className="c-cal-wrapper">
             <div className="c-cal_display">{this.state.display}</div> 
-            <button name="clear" onClick={this.clearCalculator} className="btn btn--clear">ac</button>
-            <button name="pos-neg" className="btn btn--pos-neg">+/-</button>
-            <button name="percent" className="btn btn--percent">&#37;</button>
-            <button name="period" onClick={this.pushKeyPad} className="btn btn--period">&#46;</button>    
-            <button name="one" onClick={this.pushKeyPad} className="btn btn--one">1</button>
-            <button name="two" onClick={this.pushKeyPad} className="btn btn--two">2</button>
-            <button name="three" onClick={this.pushKeyPad} className="btn btn--three">3</button>
-            <button name="four" onClick={this.pushKeyPad} className="btn btn--four">4</button>
-            <button name="five" onClick={this.pushKeyPad} className="btn btn--five">5</button>
-            <button name="six" onClick={this.pushKeyPad} className="btn btn--six">6</button>
-            <button name="seven" onClick={this.pushKeyPad} className="btn btn--seven">7</button>
-            <button name="eight" onClick={this.pushKeyPad} className="btn btn--eight">8</button>
-            <button name="nine" onClick={this.pushKeyPad} className="btn btn--nine">9</button>
-            <button name="zero" onClick={this.pushKeyPad} className="btn btn--zero">0</button>            
-            <button name="divide" onClick={this.divide} className="btn btn--divide">&#247;</button>
-            <button name="multiply" onClick={this.multiply} className="btn btn--multiply">&#215;</button>
-            <button name="subtract" onClick={this.subtract} className="btn btn--subtract">&#8722;</button>
-            <button name="add" onClick={this.add} className="btn btn--add">&#43;</button>
-            <button name="equal" onClick={this.equal} className="btn btn--equal">&#61;</button>
+            <button type="button" name="clear" onClick={this.clearCalculator} className="btn btn--clear">ac</button>
+            <button type="button" name="pos-neg" className="btn btn--pos-neg">+/-</button>
+            <button type="button" name="percent" className="btn btn--percent">&#37;</button>
+            <button type="button" name="period" onClick={this.pushKeyPad} className="btn btn--period">&#46;</button>    
+            <button type="button" name="one" onClick={this.pushKeyPad} className="btn btn--one">1</button>
+            <button type="button" name="two" onClick={this.pushKeyPad} className="btn btn--two">2</button>
+            <button type="button" name="three" onClick={this.pushKeyPad} className="btn btn--three">3</button>
+            <button type="button" name="four" onClick={this.pushKeyPad} className="btn btn--four">4</button>
+            <button type="button" name="five" onClick={this.pushKeyPad} className="btn btn--five">5</button>
+            <button type="button" name="six" onClick={this.pushKeyPad} className="btn btn--six">6</button>
+            <button type="button" name="seven" onClick={this.pushKeyPad} className="btn btn--seven">7</button>
+            <button type="button" name="eight" onClick={this.pushKeyPad} className="btn btn--eight">8</button>
+            <button type="button" name="nine" onClick={this.pushKeyPad} className="btn btn--nine">9</button>
+            <button type="button" name="zero" onClick={this.pushKeyPad} className="btn btn--zero">0</button>            
+            <button type="button" name="divide" onClick={this.divide} className="btn btn--divide">&#247;</button>
+            <button type="button" name="multiply" onClick={this.multiply} className="btn btn--multiply">&#215;</button>
+            <button type="button" name="subtract" onClick={this.subtract} className="btn btn--subtract">&#8722;</button>
+            <button type="button" name="add" onClick={this.add} className="btn btn--add">&#43;</button>
+            <button type="button" name="equal" onClick={this.equal} className="btn btn--equal">&#61;</button>
           </div>
         </section>
     )
